e203ed6cf871d9e8edc25945cd69dc9813594134
Barry3D <frederik.guenther@web.de>
2021-09-18 15:18:43 +0200

    Fixed Annotations

---
+++ src/Controller/SignUpController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/signup", "signup")
+ *
+ * Validates a sign up request and creates a new user
+ *
+ * The request needs to contain the following to be valid:
+ *  - a user name
+ *  - an email-address
+ *  - a password that has be deemed "secure"
+ */
+class SignUpController
+{
+    public function __invoke(): JsonResponse
+    {
+        return new JsonResponse('success');
+    }
+}
