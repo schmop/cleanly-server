d476dd153d9b52b7614dc9e3a09cd3f34210ff3f
Schmop <lars.richard@rocketmail.com>
2022-08-12 01:20:43 +0200

    Add debugging commands

---
+++ src/Command/TestGetDevicesByUserCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use App\Household\HouseholdRepository;
+use App\Push\DeviceRepository;
+
+#[AsCommand('cleanly:test-get-devices-by-user')]
+class TestGetDevicesByUserCommand extends Command
+{
+    public function __construct(
+        private readonly DeviceRepository $deviceRepository,
+        private readonly HouseholdRepository $householdRepository,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $userId = $input->getFirstArgument();
+        if (null === $userId) {
+            $output->writeln('No userId given!');
+            return Command::FAILURE;
+        }
+        dump(
+            $this->deviceRepository->findBy([
+                'user_id' => $userId
+            ])
+        );
+        return Command::SUCCESS;
+    }
+}
