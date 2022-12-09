760cf36fd595aecf1eafc9878bc0e6fb3c93662e
Schmop <lars.richard@rocketmail.com>
2022-12-09 15:06:53 +0100

    Update settings controller to use new json format

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -7,26 +7,35 @@ namespace App\Controller;
 use App\HttpFoundation\JsonErrorResponse;
 use App\HttpFoundation\JsonSuccessResponse;
 use App\Json\Json;
-use App\User\Entity\UserSettings;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\User\UserFetcher;
 use App\User\UserSettingsData;
 use App\User\UserSettingsRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 #[Route("/api/user/settings", "user_settings")]
 class SettingsController
 {
-    public function __invoke(Request $request, UserFetcher $userFetcher, UserSettingsRepository $userSettingsRepository): Response
-    {
+    public function __invoke(
+        Request $request,
+        UserFetcher $userFetcher,
+        UserSettingsRepository $userSettingsRepository,
+        LoggerInterface $logger,
+    ): Response {
         $user = $userFetcher->getUser();
         $settings = $user->getUserSettings();
         try {
-            $settingsData = UserSettingsData::createFromJson(Json::fromRequest($request)->json('settings'));
+            $settingsData = UserSettingsData::createFromJson(Json::fromRequest($request));
             $settingsData->applyTo($settings);
             $userSettingsRepository->save($settings);
         } catch (\Exception $e) {
+            $logger->error('Could not save settings, reason: {reason}', [
+                'reason' => $e->getMessage(),
+                'exception' => $e
+            ]);
+
             return JsonErrorResponse::create(['reason' => $e->getMessage()]);
         }
 
