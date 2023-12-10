8757d463441348860146201402c32feb10fc3f03
Schmop <lars.richard@rocketmail.com>
2023-12-10 01:12:24 +0100

    Do not die on failing whoami

--- hub/whoami.ts
+++ hub/whoami.ts
@@ -2,15 +2,20 @@ import fetch from 'node-fetch';
 
 export async function whoami(token: string): Promise<null|number> {
     const host = process.env.NODE_ENV === 'production' ? 'https://cleanly.schmoppo.de' : 'http://localhost:8000';
-    const response = await fetch(`${host}/api/whoami`, {
-        headers: {
-            'Authorization': `Bearer ${token}`,
+    try {
+        const response = await fetch(`${host}/api/whoami`, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        });
+        if (response.status !== 200) {
+            console.error('Could not retrieve user id from auth header!');
+            return null;
         }
-    });
-    if (response.status !== 200) {
-        console.error('Could not retrieve user id from auth header!');
+
+        return parseInt(await response.text());
+    } catch (err) {
+        console.error(err);
         return null;
     }
-
-    return parseInt(await response.text());
 }
