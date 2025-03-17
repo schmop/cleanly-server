6548b15d094d42955220eb78271e662bd2eff00d
schmop <lars.richard@rocketmail.com>
2025-03-17 14:47:18 +0100

    Add TestSendMail command

---
+++ src/Command/TestSendMail.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use App\Push\DeviceRepository;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+
+#[AsCommand('cleanly:test-send-mail')]
+class TestSendMail extends Command
+{
+    public function __construct(
+        private readonly MailerInterface     $mailer,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void {
+        $this->addArgument('mail', InputArgument::REQUIRED, 'Mail-address to send mail to');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $mail = $input->getArgument('mail');
+        if (null === $mail) {
+            $output->writeln('No mail address given!');
+            return Command::FAILURE;
+        }
+        $email = (new Email())
+            ->from(new Address('noreply@schmoppo.de', 'Cleanly Bot'))
+            ->to($mail)
+            ->subject('Your registration on cleanly')
+            ->text(<<<TEXT
+<p>Hello!</p>
+
+<p>This is a test mail.</p>
+
+<p>Greetings,</br>
+Cleanly.</p>
+TEXT
+)
+        ;
+
+        $this->mailer->send($email);
+        return Command::SUCCESS;
+    }
+}
